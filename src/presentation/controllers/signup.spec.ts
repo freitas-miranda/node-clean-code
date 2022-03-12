dce7d844f6112508efa9273007107da8da20b708
Alan Miranda <ti040@irmaosgoncalves.com.br>
2022-03-12 17:30:10 -0400

    test: garantir que seja retornado erro 400 no caso de nao informar o nome

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,31 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Deve retornar 400 se o nome não for informado', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Sem parametro: nome'))
+  })
+
+  test('Deve retornar 400 se o email não for informado', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Sem parametro: email'))
+  })
+})
