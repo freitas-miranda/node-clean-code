0839f3a621931ade87c567601e68f5fe3e2b2a91
Alan Miranda <ti040@irmaosgoncalves.com.br>
2022-03-20 17:37:41 -0400

    test: garantir que BcryptAdapter chame bcrypt com valores corretos

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+const salt = 12
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Garantir que bcrypt será chamada com valor correto', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.hash('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
