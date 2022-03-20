58aca1c62771ac415223fa92c78b10e7072116da
Alan Miranda <ti040@irmaosgoncalves.com.br>
2022-03-20 17:42:21 -0400

    test: deve retornar um hash válido no sucesso do hash

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,16 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return 'hash'
+  },
+
+  async compare (): Promise<boolean> {
+    return true
+  }
+}))
+
 const salt = 12
 const makeSut = (): BcryptAdapter => {
   return new BcryptAdapter(salt)
@@ -12,5 +22,11 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Deve retornar um hash válido no sucesso do hash', async () => {
+    const sut = makeSut()
+    const hash = await sut.hash('any_value')
+    expect(hash).toBe('hash')
   })
 })
