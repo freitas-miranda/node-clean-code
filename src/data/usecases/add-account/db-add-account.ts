fa75638c0203d2c5516928ca2dc7d0400c057d3c
Alan Miranda <ti040@irmaosgoncalves.com.br>
2022-03-20 17:10:22 -0400

    feat: garantir que o retorno de add account retorne sucesso

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,14 +1,17 @@
-import { AccountModel, AddAccount, AddAccountModel, Encrypter } from './db-add-account-protocols'
+import { AccountModel, AddAccount, AddAccountModel, AddAccountRespository, Encrypter } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRespository: AddAccountRespository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRespository: AddAccountRespository) {
     this.encrypter = encrypter
+    this.addAccountRespository = addAccountRespository
   }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
-    return new Promise(resolve => resolve(null))
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    const account = await this.addAccountRespository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    return account
   }
 }
