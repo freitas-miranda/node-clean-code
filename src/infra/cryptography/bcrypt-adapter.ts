6f5a24751de53f305148a0266bd75e39f6c7e5d1
Alan Miranda <ti040@irmaosgoncalves.com.br>
2022-03-29 20:16:55 -0400

    feat: adiciona SignUp Factory

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,8 +1,13 @@
 import bcrypt from 'bcrypt'
-import { HashComparer, Hasher } from '../../data/protocols/cryptography'
+// import { HashComparer, Hasher } from '../../data/protocols/cryptography'
+import { Encrypter } from '../../data/protocols/encrypter'
 
-export class BcryptAdapter implements Hasher, HashComparer {
+export class BcryptAdapter implements Encrypter {
   constructor (private readonly salt: number) {}
+
+  async encrypt (plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt)
+  }
 
   async hash (plaintext: string): Promise<string> {
     return bcrypt.hash(plaintext, this.salt)
